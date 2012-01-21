8180555e4846257c9bf0ad18ad911ef16e6a1935
jonathan <naruto137@gmail.com>
2012-01-21 10:54:38 -0500

    moved to method so that it returns a string

--- src/edu/upenn/cis/cis555/hw0/HelloWorld.java
+++ src/edu/upenn/cis/cis555/hw0/HelloWorld.java
@@ -1,7 +1,12 @@
 package edu.upenn.cis.cis555.hw0;
 
 public class HelloWorld {
+	
+	public static String writeName() {
+		return "Jonathan Leung (jonleung)";
+	}
+	
 	public static void main(String[] args) {
-		System.out.println("Jonathan Leung (jonleung)");
+		HelloWorld.writeName();
 	}
 }
--- src/test/edu/upenn/cis/cis555/hw0/HelloWorldTest.java
+++ src/test/edu/upenn/cis/cis555/hw0/HelloWorldTest.java
@@ -1,9 +1,10 @@
 package test.edu.upenn.cis.cis555.hw0;
 
+import edu.upenn.cis.cis555.hw0.HelloWorld;
 import junit.framework.TestCase;
 
 public class HelloWorldTest extends TestCase {
   public void testA() {
-	  assertTrue(true);
+	  assertTrue(HelloWorld.writeName() == "Jonathan Leung (jonleung)");
   }
 }
